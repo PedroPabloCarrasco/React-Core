aff57624642b5762ccb2048acaefe78da86b017a
PedroPablo <pedropablocarrascob@gmail.com>
2025-03-14 19:16:44 -0300

    Agregando estilos correctamente

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TarjetaFruta from './componentes/TarjetaFruta';
+
+
+function App() {
+    return (
+        <div>
+            <h1>Bienvenido a mi aplicación React</h1>
+            <TarjetaFruta /> 
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // Importa ReactDOM desde 'react-dom/client' para React 18 y superior
+import App from './App'; // Asegúrate de que el archivo App.js exista en la misma carpeta
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// Crear un contenedor para la aplicación
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Renderizar el componente App en el contenedor
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
