0e7ced25ade8689552bdb57a7635010cee715450
PedroPablo <pedropablocarrascob@gmail.com>
2025-03-14 12:53:44 -0300

    Agregando archivo de estilos y estilos globales

--- src/componentes/tarjetaFruta.js
+++ src/componentes/tarjetaFruta.js
@@ -1,51 +1,51 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import './TarjetaFruta.css'
 
 class TarjetaFruta extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            cantidad: 0
-        };
-        this.agregar = this.agregar.bind(this);
-        this.quitar = this.quitar.bind(this);
-        this.limpiar = this.limpiar.bind(this);
-    }
+  state = {
+    cantidad: 0
+  }
 
-    agregar() {
-        this.setState({ cantidad: this.state.cantidad + 1 });
-    }
+  agregar = () => {
+    this.setState({
+      cantidad: this.state.cantidad + 1
+    })
+  }
 
-    quitar() {
-        this.setState({ cantidad: this.state.cantidad - 1 });
-    }
+  quitar = () => {
+    this.setState({
+      cantidad: this.state.cantidad - 1
+    })
+  }
 
-    limpiar() {
-        this.setState({ cantidad: 0 });
-    }
+  limpiar = () => {
+    this.setState({
+      cantidad: 0
+    })
+  }
 
-    render() {
-        return (
-            <div>
-                <h3>{this.props.name}</h3>
-                <div>Cantidad {this.state.cantidad}</div>
-                <button onClick={this.agregar}>+</button>
-                <button onClick={this.quitar}>-</button>
-                <button onClick={this.limpiar}>Limpiar</button>
-                <hr />
-                <p>$ {this.props.price}</p>
-                <p>Total: $ {this.props.price * this.state.cantidad}</p>
-            </div>
-        );
-    }
+  render () {
+    const hasItems = this.state.cantidad > 0
+    const activeClass = hasItems ? 'TarjetaFruta-activa' : ''
+    const clases = 'TarjetaFruta ' + activeClass
+    // 'TarjetaFruta'
+    // 'TarjetaFruta TarjetaFruta-activa'
+
+    return (
+      <div className={clases}>
+        <h3>{ this.props.name }</h3>
+        <div>Cantidad: { this.state.cantidad }</div>
+        <button onClick={this.agregar}>+</button>
+        <button onClick={this.quitar}>-</button>
+        <button onClick={this.limpiar}>Limpiar</button>
+        <hr/>
+        <p>$ { this.props.price }</p>
+        <p>
+          Total: ${ this.props.price * this.state.cantidad }
+        </p>
+      </div>
+    )
+  }
 }
 
-const App = () => (
-    <div>
-        <TarjetaFruta name="Sandia" price={2.00} />
-        <TarjetaFruta name="Naranja" price={3.00} />
-        <TarjetaFruta name="Kiwi" price={4.00} />
-    </div>
-);
-
-ReactDOM.render(<App />, document.getElementById('root'));
+export default TarjetaFruta
--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-const name = 'Mundo';
-let App = <h1>Hola {name}</h1>
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-
-const user1 = {
-    name: 'Juan',
-    age: 25
-};
-
-
-
-App = <h1>Hola {user1.name} Años</h1>
-ReactDOM.render(App, root);
+ReactDOM.render(<App />, document.getElementById('root'))
